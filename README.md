229e548f54b88b2eb32c1b1397c56e750185c024
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2021-12-21 13:39:17 +0600

    implemented generic exception handling.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # web-services
+
+<h2>Annotations</h2>:
+    <p><b>@Autowired</b> - it auto wires need dependency</p>
+    <p><b>@RestController</b> - it's same as RestController and ResponseBody annotation</p>
+    <p><b>@ResponseStatus</b> - if we need to change status of HTTP response</p>
+    <p><b>@ExceptionHandler</b> - For custom exception handling we extend *ResponseEntityExceptionHandler* and 
+            annotate method with @ExceptionHandler.
+    </p>
---
+++ src/main/java/com/love2code/example/exception/CustomGenericExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.love2code.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomGenericExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ex.getMessage(), new Date());
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(CarNotFoundException.class)
+    public final ResponseEntity<Object> handleCarNotFoundException(CarNotFoundException ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ex.getMessage(), new Date());
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
